6239b867d93608517b21844b500cd80e79e94c75
Leroy Rosales <leroyrosales@gmail.com>
2021-04-03 14:02:31 -0500

    Init header component and install tailwind

---
+++ pages/components/Header.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Header() {
+    return (
+        <header className="mb-2">
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <hr className="h-2 bg-green-800"/>
+            <div className="container-lg mx-auto pt-2 pb-1 px-4 lg:px-0">
+                <h1 className="text-4xl font-semibold m-0 text-green-800">
+                    <Link href="/">
+                        <a className="hover:opacity-75">👨🏾‍💻 Leroy Rosales</a>
+                    </Link>
+                </h1>
+            </div>
+        </header>
+    )
+}
